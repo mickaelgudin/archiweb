67e4e5e4134e1b23da253110070a0062e674e0ea
mickaelgudin <mickaelgudin@gmail.com>
2021-04-01 23:15:02 +0200

    add tendancy component

--- src/components/journeyTendancy.vue
+++ src/components/journeyTendancy.vue
@@ -1,16 +1,16 @@
 <template>
-    <div id="recherche" v-if="reponse" style="margin-top:2%; margin-left:2%; margin-right:2%;">
+    <div id="recherche" style="margin-top:2%; margin-left:2%; margin-right:2%;">
         <v-row justify="center">
             <v-col cols="12" sm="12" md="10" lg="6" xl="6" >
                 <h1 style="margin-bottom: 2%">Tendances des prix sur le trajet sélectionné</h1>
                 <v-card style="border: 2px solid #60378c; margin-bottom:10%">
                     <v-list-item-group color="#60378c">
                         <v-list-item>
+                            <h2>Sur tous les trajets existants entre les gares sélectionnés</h2>
                             <v-list-item-content>
-                                <v-list-item-icon style="margin-bottom:5%">
-                                <v-icon class="iconify" :data-icon="icon" style="color:#60378c; height:70px"></v-icon>
+                                <v-list-item-icon style="margin-left: 10%;">
+                                    <v-icon :class="'iconify '+classeCss" :data-icon="icon" x-large></v-icon>
                                 </v-list-item-icon>
-                                </h5>
                             </v-list-item-content>
                         </v-list-item>
                     </v-list-item-group>
@@ -22,13 +22,10 @@
 
 <script>
 //import axios from 'axios'
-import toast from '../components/toast.vue'
   export default { 
-    components: { toast },
     data: () => ({
-      selectedGareDepart: '',
-      selectedGareArrivee : '',
-      icon : 'mdi-arrow-right' //by default stable
+      icon : 'mdi-arrow-right', //by default stable
+      classeCss : 'stable' //see css classes below
     }),
     methods: {
         /**
@@ -38,9 +35,23 @@ import toast from '../components/toast.vue'
         * @return {String}      formatted element of date
         */
         getTendancy(idStationDepart, idStationArrival) {
-            
+            console.log(idStationDepart+idStationArrival);
+
+
         } 
       
     }
   }
 </script>
+
+<style>
+    .decreasing {
+        color: red !important;
+    }
+    .increasing {
+        color: green !important;
+    }
+    .stable {
+        color: blue !important;
+    }
+</style>
